fb6e6210cb5829d1c948ea0cb7ef4ec2d7118e50
Neizmailova Natalia <natata.neizmailova@gmail.com>
2023-01-05 16:17:14 +0200

    correct-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,30 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(timeSave, 1000));
+
+function timeSave() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+console.log(
+  'localStorage.getItem(videoplayer-current-time)',
+  localStorage.getItem('videoplayer-current-time')
+);
+/*
+
+import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
@@ -9,3 +35,4 @@ player.on('play', function() {
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
+*/
